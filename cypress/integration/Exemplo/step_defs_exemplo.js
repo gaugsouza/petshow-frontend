4c1f4abc42de49064ceddd86b7ae990b265cc74c
calebe <calebe.fazzani.facul@gmail.com>
2020-04-22 19:18:48 -0300

    #11 - Configuraçao inicial codacy

--- cypress/integration/Exemplo/step_defs_exemplo.js
+++ cypress/integration/Exemplo/step_defs_exemplo.js
@@ -1,10 +1,10 @@
 /// <reference types="Cypress" />
 import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 
-Given(`entrei na página inicial`, ()=> {
+Given(`entrei na página inicial`, () => {
     cy.visit('/');
 });
 
-Then(`devo ver {string} no título.`, (titulo)=> {
+Then(`devo ver {string} no título.`, (titulo) => {
     cy.title().should('eq', titulo);
 });
--- cypress/integration/cucumber/Exemplo.feature
+++
@@ -1,5 +0,0 @@
-Feature: Exemplo
-    Exemplo de utilização do cypress
-
-    Given entrei na página inicial
-    Then devo ver "PetshowFrontend" no título.
