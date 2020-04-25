26e07a96ca04eb3eb349a32e0be03308933a0fd1
calebe <calebe.fazzani.facul@gmail.com>
2020-04-25 11:00:15 -0300

    #11 - Add configuraçoes nyc + coverage cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -18,4 +18,7 @@
 const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
+  require('@cypress/code-coverage/task')(on, config)
+
+  return config
 }
