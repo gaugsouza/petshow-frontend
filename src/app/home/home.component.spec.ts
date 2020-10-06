5557db397d6f4cedae2f49edca4b17719bd275a6
Calebe <calebe.fazzani.facul@gmail.com>
2020-10-06 17:32:34 -0300

    sem numero - correçoes em testes

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -3,7 +3,20 @@ import 'zone.js/dist/zone'
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
-import { Router } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { LoggerTestingModule } from 'ngx-logger/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { ServicosService } from '../servicos/servicos.service';
+import { Router, ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
+import { LocalStorageService } from '../servicos/local-storage.service';
+
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -13,7 +26,21 @@ describe('HomeComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ HomeComponent ],
       providers: [
-        {provide: Router, useValue: {navigate: () => true}}
+        {provide: Router, useValue: {navigate: () => true}},
+        ServicosService,
+        LocalStorageService
+      ],
+      imports: [
+        MatListModule,
+        MatInputModule,
+        MatSelectModule,
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        LoggerTestingModule,
+        FormsModule,
+        RouterTestingModule,
+        ReactiveFormsModule,
+        TranslateModule.forRoot()
       ]
     })
     .compileComponents();
