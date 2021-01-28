a1a88d8ad393657f39c0ad5a808ecda592119070
Thiago X <thiago.xra@gmail.com>
2021-01-27 22:19:57 -0300

    corrigindo testes

--- src/app/perfis/perfil-prestador/perfil-prestador.component.spec.ts
+++ src/app/perfis/perfil-prestador/perfil-prestador.component.spec.ts
@@ -27,6 +27,7 @@ import { MensagemAtivacaoComponent } from '../mensagem-ativacao/mensagem-ativaca
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { NotificationService } from '../../servicos/notification.service';
 import { ServicoDetalhado } from '../../interfaces/servico-detalhado';
+import { AgendaPrestadorComponent } from '../agenda-prestador/agenda-prestador.component';
 
 describe('PerfilPrestadorComponent', () => {
   let component: PerfilPrestadorComponent;
@@ -41,7 +42,8 @@ describe('PerfilPrestadorComponent', () => {
         InformacoesPessoaisComponent,
         EnderecoComponent,
         EnderecoDialogComponent,
-        MensagemAtivacaoComponent
+        MensagemAtivacaoComponent,
+        AgendaPrestadorComponent
        ],
        providers: [
          PrestadorService,
