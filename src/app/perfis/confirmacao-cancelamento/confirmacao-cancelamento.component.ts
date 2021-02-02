7defc19c8bc5f2d859d530460cf4b32a8a5d579c
Thiago X <thiago.xra@gmail.com>
2021-02-01 22:00:30 -0300

    tentativa caixinha

--- src/app/perfis/confirmacao-cancelamento/confirmacao-cancelamento.component.ts
+++ src/app/perfis/confirmacao-cancelamento/confirmacao-cancelamento.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Agendamento } from 'src/app/interfaces/agendamento';
+import { StatusAgendamento } from 'src/app/interfaces/statusAgendamento';
+import { AgendamentoService } from 'src/app/servicos/agendamento.service';
+import { LocalStorageService } from 'src/app/servicos/local-storage.service';
 
 @Component({
   selector: 'app-confirmacao-cancelamento',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmacaoCancelamentoComponent implements OnInit {
 
-  constructor() { }
+  agendamento: Agendamento;
+  statusAgendamento: StatusAgendamento[];
+  confirmacaoCancelamento: Boolean;
+
+  constructor(
+    private agendamentoService:AgendamentoService,
+    private localStorageService:LocalStorageService,
+  ) { }
 
   ngOnInit(): void {
   }
+
+  botaoConfirmaCancelamento() {
+    this.confirmacaoCancelamento = !this.confirmacaoCancelamento;
+  }
+
+  // cancelaAgendamento(agendamento){
+  //   const cancelaId = this.statusAgendamento.find(status => status.nome.toLowerCase().indexOf("cancelado")!==-1)
+  //   this.localStorageService.getItem(USER_TOKEN).subscribe((token : string) => {
+  //     this.agendamentoService.alterarStatusAgendamento(agendamento.prestadorId, cancelaId.id, agendamento.id, token)
+  //     .subscribe(() => {
+  //       this.buscarAgendamentosPorCliente(this.clienteId, this.paginaAtual, this.quantidadeItens);
+  //     })
+  //   });
+  // }
 
 }
