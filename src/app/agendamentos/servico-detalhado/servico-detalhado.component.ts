497c66d8a8ce25ffa213f20f95768030286a33f1
Thiago X <thiago.xra@gmail.com>
2021-02-07 13:50:11 -0300

    corrigido data em pt em agendamento

--- src/app/agendamentos/servico-detalhado/servico-detalhado.component.ts
+++ src/app/agendamentos/servico-detalhado/servico-detalhado.component.ts
@@ -12,41 +12,18 @@ import { Adicional } from 'src/app/interfaces/adicional';
 import {FormControl} from '@angular/forms';
 import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
 import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
-// Depending on whether rollup is used, moment needs to be imported differently.
-// Since Moment.js doesn't have a default export, we normally need to import using the `* as`
-// syntax. However, rollup creates a synthetic default module and we thus need to import it using
-// the `default as` syntax.
-import * as _moment from 'moment';
-// tslint:disable-next-line:no-duplicate-imports
-import {default as _rollupMoment} from 'moment';
-
-
-const moment = _rollupMoment || _moment;
-
-export const MY_FORMATS = {
-  parse: {
-    dateInput: 'LL',
-  },
-  display: {
-    dateInput: 'LL',
-    monthYearLabel: 'MMM YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY',
-  },
-};
 
 @Component({
   selector: 'app-servico-detalhado',
   templateUrl: './servico-detalhado.component.html',
   styleUrls: ['./servico-detalhado.component.scss'],
   providers: [
-    {provide: MAT_DATE_LOCALE, useValue: 'ja-JP'},
+    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},
     {
       provide: DateAdapter,
       useClass: MomentDateAdapter,
       deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
     },
-    // {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
     {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
   ],
 })
@@ -68,18 +45,12 @@ export class ServicoDetalhadoComponent implements OnInit {
   adicionais:Adicional[] = [];
   dataAgendamento:Date;
   dataMinima:Date;
-  date = new FormControl(moment());
 
-  constructor(private _adapter: DateAdapter<any>,
+
+  constructor(
     private localStorageService: LocalStorageService,
-    private servicosService: ServicosService) { }
-
-    english() {
-      this._adapter.setLocale('en');
-    }
-    portuguese() {
-      this._adapter.setLocale('pt');
-    }
+    private servicosService: ServicosService,
+    private _adapter: DateAdapter<any>) { }
 
   ngOnInit(): void {
     this.dataMinima = new Date();
