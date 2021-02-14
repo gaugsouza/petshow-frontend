c1a12ec12ceb60b7af2513302890c30c82312a04
calebe <calebe.fazzani.facul@gmail.com>
2021-02-14 10:47:12 -0300

    #172 - adiçao de componentes de geolocalizacao

---
+++ src/app/geolocalizacao/geolocalizacao.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GeolocalizacaoComponent } from './geolocalizacao/geolocalizacao.component';
+
+
+
+@NgModule({
+  declarations: [GeolocalizacaoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GeolocalizacaoModule { }
---
+++ src/app/geolocalizacao/geolocalizacao/geolocalizacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-geolocalizacao',
+  templateUrl: './geolocalizacao.component.html',
+  styleUrls: ['./geolocalizacao.component.scss']
+})
+export class GeolocalizacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
