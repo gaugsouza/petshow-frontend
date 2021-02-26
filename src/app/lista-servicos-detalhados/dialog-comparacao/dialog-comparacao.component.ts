d7dc57a8ba0b91d59d99219ddf96dc058dbd4b35
calebe <calebe.fazzani.facul@gmail.com>
2021-02-25 22:11:30 -0300

    Correção em exibição de texto traduzido em modal de comparação

--- src/app/lista-servicos-detalhados/dialog-comparacao/dialog-comparacao.component.ts
+++ src/app/lista-servicos-detalhados/dialog-comparacao/dialog-comparacao.component.ts
@@ -61,4 +61,12 @@ export class DialogComparacaoComponent implements OnInit {
 
     this.dataSource = [...this.dataSource, ...campos];
   }
+
+  geraNomePrestador(valor:string) {
+    return valor.split('|')[1];
+  }
+
+  geraTipo(valor:string) {
+    return valor.split('|')[0];
+  }
 }
