173be07d9f92e9b4b138ad8752f05f6c4bdb1b85
calebe <calebe.fazzani.facul@gmail.com>
2021-03-13 18:22:46 -0300

    add helmet

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
+app.use(helmet());
+
 
 const pathName = `${__dirname}/dist/petshow-frontend`;
 app.use(express.static(pathName));
 
       
 app.get('/*', (_, res) => {
-    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-    res.setHeader('Content-Security-Policy', 'Content-Security-Policy: default-src https://petshow-backend.herokuapp.com; default-src http://localhost:4200');
-    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
-    res.setHeader('X-Content-Type-Options', 'nosniff always');
-    res.setHeader('Referrer-Policy', 'no-referrer');
-    res.setHeader('Permissions-Policy', 'geolocation=(self "https://petshow-backend.herokuapp.com" "http://localhost:4200")');
     res.sendFile(path.join(`${pathName}/index.html`));
 });
 
