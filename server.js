c11a0c6054714b2b598511323e82121fc2bf7d1a
calebe <calebe.fazzani.facul@gmail.com>
2021-03-13 20:24:47 -0300

    Correçao em header e adiçao de Permissions-Policy

--- server.js
+++ server.js
@@ -12,9 +12,17 @@ app.use(helmet.contentSecurityPolicy({
     directives: {
         ...helmet.contentSecurityPolicy.getDefaultDirectives(),
         "default-src": ["'self'",'https://petshow-backend.herokuapp.com', 'https://br-cidade-estado-nodejs.glitch.me', 'https://nominatim.openstreetmap.org', 'https://www.mercadopago.com.br', 'https://viacep.com.br', 'https://fonts.googleapis.com/', 'https://fonts.gstatic.com', '*.tile.openstreetmap.org'],
-        "img-src": ['*.tile.openstreetmap.org']
+        "img-src": ["'self'", '*.tile.openstreetmap.org']
     }
 }));
+
+app.use((req, res, next) => {
+    res.setHeader(
+      "Permissions-Policy",
+      'geolocation=(self), microphone=()'
+    );
+    next();
+});
 
 const pathName = `${__dirname}/dist/petshow-frontend`;
 app.use(express.static(pathName));
